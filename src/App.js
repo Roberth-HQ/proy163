75114318ca84da73747f02e6eb98a20324880131
Roberth-HQ <rocahuqui@gmail.com>
2024-04-22 00:36:32 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import MainPage from './MainPage';
+import MainPage2 from './MainPage2';
+import MainPage3 from './MainPage3';
+import MainPage4 from './MainPage4';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+              <Route exact path="/proy163" element={<LoginForm />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/MainPage" element={<MainPage />} />
+                <Route path="/MainPage2" element={<MainPage2 />} />
+                <Route path="/MainPage3" element={<MainPage3 />} />
+                <Route path="/MainPage4" element={<MainPage4 />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
